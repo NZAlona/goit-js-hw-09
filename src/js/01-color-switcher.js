7d4acd9a07b15417ed4d9bd8aa01e8c6057efbbc
NZAlona <amakoveychenko@gmail.com>
2022-12-23 09:34:18 +0200

    Adds first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+btnStart.addEventListener('click', onStartBtnClick);
+btnStop.addEventListener('click', onStopBtnClick);
+
+let timerId = null;
+
+function onStartBtnClick() {
+  timerId = setInterval(() => {
+    const randomColorPicker = getRandomHexColor();
+    body.style.background = randomColorPicker;
+  }, 1000);
+  btnStart.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', true);
+}
